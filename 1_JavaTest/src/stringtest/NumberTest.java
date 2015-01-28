603d96b0c45e2e74b68214ffa2d2e3c9980753bf
sinlov <sinlovgmppt@gmail.com>
2015-01-28 14:31:51 +0800

    some test number

--- 1_JavaTest/src/stringtest/NumberTest.java
+++ 1_JavaTest/src/stringtest/NumberTest.java
@@ -4,15 +4,19 @@ public class NumberTest {
     public static void main(String[] args) {
         int a = Integer.MAX_VALUE >> 2;
         int b = Integer.MAX_VALUE;
-        System.out.println(a);
-        System.out.println(b);
-        System.out.println(b);
-        System.out.println(b);
-        System.out.println(a);
-        System.out.println(a);
-        for(int i = 0; i < 100; i++){
-            System.out.println(1 << i);
-        }
+        int c = 1280;
+        int d = 60;
+        float fMultiple = (float)d / 100 ;
+        int e = (int) (c* fMultiple);
+        System.out.println(e);
+//        System.out.println(b);
+//        System.out.println(b);
+//        System.out.println(b);
+//        System.out.println(a);
+//        System.out.println(a);
+//        for(int i = 0; i < 100; i++){
+//            System.out.println(1 << i);
+//        }
             
     }  
 }
